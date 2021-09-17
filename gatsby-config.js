7b2d5d9b4aa71164a864c9c0e1d95c95bdef7bac
Janice Lee <jlad2017@mymail.pomona.edu>
2021-09-17 13:10:31 -0700

    Configure Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,36 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `a cozy space`,
+    description: `This is my website.`,
+    author: {
+      name: `Janice Lee`,
+      email: `janice.lee@pomona.edu`
+    },
+    social: {
+      github: `https://github.com/lee-janice`
+    },
+    siteUrl: `https://acozy.space.com`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
+    // custom content for head of page
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
+    // filesystem 
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: `posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,18 +38,63 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    // remark transformer to convert markdown to html
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // autosizes images
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280,
+            },
+          },
+          // autoresizes iframes
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          // adds link for each header in a post  
+          `gatsby-remark-autolink-headers`,
+          // syntax highlighting for code blocks
+          `gatsby-remark-prismjs`,
+          // copies externally linked files to project on build
+          `gatsby-remark-copy-linked-files`,
+          // converts quotes/apostrophes to smart quotes/apostrophes
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    // enhances and resizes images
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // favicon
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `a cozy space`,
+        short_name: `acozy.space`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://acozy.space.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
