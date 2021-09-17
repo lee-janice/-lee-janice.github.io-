a806955f9b0d2a481b4eabaab37af06dbe049c8c
Janice Lee <jlad2017@mymail.pomona.edu>
2021-09-17 14:07:10 -0700

    Hello wworld

---
+++ src/pages/about.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+interface Props {
+    readonly data: PageQueryData
+}
+
+const About: React.FC<Props> = ({ data }) => {
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+        <Layout title={siteTitle}>
+            <Head title="All tags" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+            <article>About Jeff Rafter...</article>
+        </Layout>
+    )
+}
+
+interface PageQueryData {
+    site: {
+        siteMetadata: {
+            title: string
+        }
+    }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+export default About
